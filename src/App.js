b9b552fb860ce3faef2a850024b1b09dbe358bfe
IuliiaSelivanova <yulya.danilina.96@inbox.ru>
2024-12-24 21:07:12 +0300

    header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Header from "./components/ui/header/Header";
 
 function App() {
   return (
-    <div className="App">
-    
+    <div className="app">
+      <Header/> 
     </div>
   );
 }
